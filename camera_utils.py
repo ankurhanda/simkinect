9c424803cf73d326c3388b27cf788742b2e2d2f5
Ankur Handa <ahanda@nvidia.com>
2020-07-19 00:15:00 -0700

    added jbohg render kinect blensor noise model
    Signed-off-by: Ankur Handa <ahanda@nvidia.com>

--- camera_utils.py
+++ camera_utils.py
@@ -90,10 +90,10 @@ def normal_from_vertex(vertices):
 
     return axb 
 
-def add_gaussian_shifts(depth):
+def add_gaussian_shifts(depth, std=1/2.0):
 
     rows, cols = depth.shape 
-    gaussian_shifts = np.random.normal(0, 1/2.0, size=(rows, cols, 2))
+    gaussian_shifts = np.random.normal(0, std, size=(rows, cols, 2))
     gaussian_shifts = gaussian_shifts.astype(np.float32)
 
     # creating evenly spaced coordinates  
@@ -114,7 +114,92 @@ def add_gaussian_shifts(depth):
     return depth_interp
     
 
+def filterDisp(disp, dot_pattern_):
+
+    invalid_disp_ = 99999999.9
+
+    size_filt_ = 9
+
+    xx = np.linspace(0, size_filt_-1, size_filt_)
+    yy = np.linspace(0, size_filt_-1, size_filt_)
+
+    xf, yf = np.meshgrid(xx, yy)
+
+    xf = xf - int(size_filt_ / 2.0)
+    yf = yf - int(size_filt_ / 2.0)
+
+    sqr_radius = (xf**2 + yf**2)
+    vals = sqr_radius * 1.2**2 
+
+    vals[vals==0] = 1 
+    weights_ = 1 /vals  
+
+    fill_weights = 1 / ( 1 + sqr_radius)
+    fill_weights[sqr_radius > 9] = -1.0 
+
+    disp_rows, disp_cols = disp.shape 
+    dot_pattern_rows, dot_pattern_cols = dot_pattern_.shape
+
+    lim_rows = np.minimum(disp_rows - size_filt_, dot_pattern_rows - size_filt_)
+    lim_cols = np.minimum(disp_cols - size_filt_, dot_pattern_cols - size_filt_)
+
+    center = int(size_filt_ / 2.0)
+
+    window_inlier_distance_ = 0.1
+
+    out_disp = np.ones_like(disp) * invalid_disp_
+
+    interpolation_map = np.zeros_like(disp)
+
+    for r in range(0, lim_rows):
+
+        for c in range(0, lim_cols):
+
+            if dot_pattern_[r+center, c+center] > 0:
+                                
+                # c and r are the top left corner 
+                window  = disp[r:r+size_filt_, c:c+size_filt_] 
+                dot_win = dot_pattern_[r:r+size_filt_, c:c+size_filt_] 
+  
+                valid_dots = dot_win[window < invalid_disp_]
+
+                n_valids = np.sum(valid_dots) / 255.0 
+                n_thresh = np.sum(dot_win) / 255.0 
+
+                if n_valids > n_thresh / 1.2: 
+
+                    mean = np.mean(window[window < invalid_disp_])
+
+                    diffs = np.abs(window - mean)
+                    diffs = np.multiply(diffs, weights_)
+
+                    cur_valid_dots = np.multiply(np.where(window<invalid_disp_, dot_win, 0), 
+                                                 np.where(diffs < window_inlier_distance_, 1, 0))
+
+                    n_valids = np.sum(cur_valid_dots) / 255.0
+
+                    if n_valids > n_thresh / 1.2: 
+                    
+                        accu = window[center, center] 
+
+                        assert(accu < invalid_disp_)
+
+                        out_disp[r+center, c + center] = round((accu)*8.0) / 8.0
+
+                        interpolation_window = interpolation_map[r:r+size_filt_, c:c+size_filt_]
+                        disp_data_window     = out_disp[r:r+size_filt_, c:c+size_filt_]
+
+                        substitutes = np.where(interpolation_window < fill_weights, 1, 0)
+                        interpolation_window[substitutes==1] = fill_weights[substitutes ==1 ]
+
+                        disp_data_window[substitutes==1] = out_disp[r+center, c+center]
+
+    return out_disp
+
 if __name__ == "__main__":
+
+
+    dot_pattern_ = cv2.imread("./data/kinect-pattern_3x3.png", 0)
 
     count = 0
 
@@ -129,16 +214,31 @@ if __name__ == "__main__":
         # depth in meters 
         depth = depth_uint16.astype('float') / 5000.0
 
-        depth_interp = add_gaussian_shifts(depth)
+        disp_= 480.0 * 0.075 / depth
 
-        # The depth here needs to converted to cms so scale factor is introduced 
-        # though often this can be tuned from [100, 200] to get the desired banding / quantisation effects 
-        noisy_depth = (35130/np.round(35130/np.round(depth_interp*scale_factor) + np.random.normal(size=(h, w))*(1.0/6.0) + 0.5))/scale_factor 
-        noisy_depth = noisy_depth * 5000.0 
+        depth_f = np.round(disp_ * 8.0)/8.0
+
+        out_disp = filterDisp(depth_f, dot_pattern_)
+
+        depth = 480.0 * 0.075 / out_disp
+        depth[out_disp == 99999999.9] = 0 
+
+
+        noisy_depth = depth * 5000.0
         noisy_depth = noisy_depth.astype('uint16')
+
+        # depth_interp = add_gaussian_shifts(depth)
+
+        # # The depth here needs to converted to cms so scale factor is introduced 
+        # # though often this can be tuned from [100, 200] to get the desired banding / quantisation effects 
+        # noisy_depth = (35130/np.round((35130/np.round(depth_interp*scale_factor)) + np.random.normal(size=(h, w))*(1.0/6.0) + 0.5))/scale_factor 
+
+        # noisy_depth = noisy_depth * 5000.0 
+        # noisy_depth = noisy_depth.astype('uint16')
 
         # Displaying side by side the orignal depth map and the noisy depth map with barron noise cvpr 2013 model
         cv2.namedWindow('Adding Kinect Noise', cv2.WINDOW_AUTOSIZE)
+        # cv2.imshow('Adding Kinect Noise', np.hstack((depth_uint16, noisy_depth)))
         cv2.imshow('Adding Kinect Noise', np.hstack((depth_uint16, noisy_depth)))
         key = cv2.waitKey(1)
 
@@ -146,5 +246,6 @@ if __name__ == "__main__":
         if key & 0xFF == ord('q') or key == 27:
             cv2.destroyAllWindows()
             break
-        
+
+        print(count)
         count = (count + 1)% 500
